45b9cd3f496d75fde81a5a7b7abe07e4bc245703
Julio Cesar <165454530+jcmljunior@users.noreply.github.com>
2024-11-17 18:24:03 -0300

    adicionado utilitário para ajustes de cores.

---
+++ lib/features/theme_manager/mixins/color_utilities_mixin.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart' show Color, HSLColor;
+
+mixin ColorUtilitiesMixin {
+  Color darken(Color color, [double amount = .1]) {
+    assert(amount >= 0 && amount <= 1);
+    final hsl = HSLColor.fromColor(color);
+    final hslLight =
+        hsl.withLightness((hsl.lightness - amount).clamp(0.0, 1.0));
+
+    return hslLight.toColor();
+  }
+
+  Color lighten(Color color, [double amount = .1]) {
+    assert(amount >= 0 && amount <= 1);
+
+    final hsl = HSLColor.fromColor(color);
+    final hslLight =
+        hsl.withLightness((hsl.lightness + amount).clamp(0.0, 1.0));
+
+    return hslLight.toColor();
+  }
+}
